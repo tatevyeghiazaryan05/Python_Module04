0ed4d97a380e8364a3bb3693f5ca2de8ef2db3be
Super User <root@DESKTOP-63BM6S7.>
2026-07-16 16:47:02 +0400

    ex2 ex3 done

--- ex2/ft_stream_management.py
+++ ex2/ft_stream_management.py
@@ -10,23 +10,17 @@ if __name__ == "__main__":
         file_name = sys.argv[1]
         print(f"Accessing file '{file_name}'")
         try:
-            file: typing.IO[str] = open(file_name)
+            file: typing.IO[str] = open(file_name, "r")
             content = file.read()
             print("---\n")
             print(content)
             print("---")
             file.close()
             print(f"File '{file_name}' closed.")
-            split_content = content.split('\n')
-            new_list = []
-            for c in split_content:
-                if c != '':
-                    new_str = c + '#'
-                    new_list.append(new_str)
-            s = ''
-            for i in new_list:
-                s += i
-                s += '\n'
+            s = ""
+            for line in content.split("\n"):
+                if line:
+                    s += line + "#\n"
             print("\nTransform data:")
             print("---\n")
             print(s)
@@ -37,15 +31,17 @@ if __name__ == "__main__":
                 saving_file_name = sys.stdin.readline()
                 saving_file_name = saving_file_name.strip()
                 if (saving_file_name):
-                    new_file: typing.IO[str] = open(saving_file_name, "w")
                     print(f"Saving data to '{saving_file_name}'")
+                    new_file: typing.IO[str] = open(saving_file_name, "w")
                     new_file.write(s)
                     new_file.close()
                     print(f"Data saved in file '{saving_file_name}'.")
                 else:
                     print("Not saving data.")
             except OSError as e:
-                sys.stderr.write(f"[STDERR] Error opening file '{saving_file_name}': {e}\n"
-                                 f"Data not saved.")
+                sys.stderr.write(f"[STDERR] Error opening file "
+                                 f"'{saving_file_name}': {e}\n")
+                print("Data not saved.")
         except OSError as e:
-            sys.stderr.write(f"[STDERR] Error opening file '{file_name}': {e}\n")
+            sys.stderr.write(f"[STDERR] Error opening file "
+                             f"'{file_name}': {e}\n")
---
+++ ex3/ft_vault_security.py
@@ -0,0 +1,32 @@
+def secure_archive(file_name: str, action: str = "read", add_line: str = None) -> tuple[bool, str]:
+    if action in  ("read", "write"):
+        if action == "read":
+            try:
+                with open(file_name, "r") as file:
+                    content = file.read()
+                    return (True, content)
+            except OSError as e:
+                return (False, str(e))
+        elif action == "write":
+            try:
+                with open(file_name, "w") as file:
+                    if add_line:
+                        file.write(add_line)
+                        return (True, "Content successfully written to file")
+                    else:
+                        return (False, "No content provided")
+            except OSError as e:
+                return (False, str(e))
+    return (False, "Invalid action")
+
+
+if __name__ == "__main__":
+    print("=== Cyber Archives Security ===")
+    print("Using 'secure_archive' to read from a nonexistent file:")
+    print(secure_archive("/non/existing/file", "read"))
+    print("\nUsing 'secure_archive' to read from an inaccessible file:")
+    print(secure_archive("/etc/master.passwd", "read"))
+    print("\nUsing 'secure_archive' to read from a regular file:")
+    print(secure_archive("ancient_fragment.txt", "read"))
+    print("\nUsing 'secure_archive' to write previous content to a new file:")
+    print(secure_archive("ancient_fragment.txt", "write", "Hello"))
